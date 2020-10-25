d72e9a388139bb99fd7866618191e1d13cbc482f
Galuh Ramadhan <galuh.html@gmail.com>
2020-10-25 07:10:01 +0700

    account menu

--- client/src/js/route/components/Header.js
+++ client/src/js/route/components/Header.js
@@ -9,19 +9,45 @@ class Header extends React.Component {
 
     this.state = {
       title: "Perpustakaan",
+      isShowingDropdown: false,
     };
   }
 
+  showDropdown = () => {
+    const { isShowingDropdown } = this.state;
+
+    this.setState({ isShowingDropdown: !isShowingDropdown });
+  }
 
   render() {
-    const { title } = this.state;
+    const { title, isShowingDropdown } = this.state;
 
     return (
-      <div className="header">
-        <div className="logo">
-            <a href="#">{title}</a>
-        </div>
-      </div>
+            <div className="header">
+                <div className="logo">
+                    <a href="#">{title}</a>
+                </div>
+                <div className="account-menu">
+                    {/* <button class="btn btn-default" type="button">
+                            <span><i className="fa fa-lock" /></span>
+                            &nbsp;
+                            Login
+                    </button> */}
+                    <div class={`dropdown ${isShowingDropdown ? "open" : ""}`}>
+                        <button class="btn btn-default dropdown-toggle" type="button" onClick={this.showDropdown}>
+                            <span><i className="fa fa-user" /></span>
+                            &nbsp;
+                            Administrator
+                            {" "}
+                            <span><i className="fa fa-angle-down" /></span>
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Ubah Password</a></li>
+                            <li><a href="#">Logout</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
     );
   }
 }
